b4c1e64068bfbdfe7405a42d8d062ebcc4a283b0
NTF <gary@newtechfusion.com>
2013-01-18 15:28:44 +0530

    initial commit add spree 1.3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackpearl::Application.config.secret_token = '35ef1fb25f15f5bc27f8792847ed52f21aa58a5abea82451807778fa97c7361081e73e01244c6635170349f528af977c66bdefc3fe3e0fbcb12a4b60f57bb49a'
